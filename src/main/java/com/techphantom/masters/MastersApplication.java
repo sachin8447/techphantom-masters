76a8e7a0da1530a1614f53f7fddeba7d9d4e4d4e
Sachin Kumar <sachin8447@gmail.com>
2022-06-03 11:01:02 +0530

    In Com

---
+++ src/main/java/com/techphantom/masters/MastersApplication.java
@@ -0,0 +1,13 @@
+package com.techphantom.masters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MastersApplication.class, args);
+    }
+
+}
